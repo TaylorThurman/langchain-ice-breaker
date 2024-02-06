140073409bf501ee0fba3c2fc5ebbe9b9343ee26
Taylor Thurman <taylor.thurman@protonmail.com>
2024-02-06 11:06:44 -0600

    added twitter processing

--- ice_breaker.py
+++ ice_breaker.py
@@ -3,28 +3,46 @@ from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from agents.twitter_lookup_agent import lookup as twitter_lookup_agent
 from third_parties.linkedin import scrape_linkedin_profile
+from third_parties.twitter import scrape_user_tweets
 
 
-if __name__ == "__main__":
-    print("Hello LangChain!")
-
+def ice_break(name: str) -> str:
     linkedin_profile_url = linkedin_lookup_agent(name="Eden Marco Udemy")
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url)
+
+    # twitter_username = twitter_lookup_agent(name=name)
+    # tweets = scrape_user_tweets(username=twitter_username)
+
+    # summary_template = """
+    #      given the Linkedin information {linkedin_information} and twitter {twitter_information} about a person from I want you to create:
+    #      1. a short summary
+    #      2. two interesting facts about them
+    #      3. A topic that may interest them
+    #      4. 2 creative ice breakers to pen a conversation with them
+    #  """
 
     summary_template = """
-         given the Linkedin information {information} about a person from I want you to create:
+         given the Linkedin information {linkedin_information} about a person from I want you to create:
          1. a short summary
          2. two interesting facts about them
+         3. A topic that may interest them
+         4. 2 creative ice breakers to pen a conversation with them
      """
 
     summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
+        input_variables=["linkedin_information"],
+        template=summary_template,
     )
 
-    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+    llm = ChatOpenAI(temperature=1, model_name="gpt-3.5-turbo")
 
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
 
-    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url)
+    print(chain.run(linkedin_information=linkedin_data))
 
-    print(chain.run(information=linkedin_data))
+
+if __name__ == "__main__":
+    ice_break(name="Eden Marco Udemy")
+    pass
