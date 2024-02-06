52a54d9b73ad04524277a8df9fddbced4309938e
Taylor Thurman <taylor.thurman@protonmail.com>
2024-02-06 11:22:17 -0600

    added output parser with pydantic

--- ice_breaker.py
+++ ice_breaker.py
@@ -4,11 +4,12 @@ from langchain.chains import LLMChain
 
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 from agents.twitter_lookup_agent import lookup as twitter_lookup_agent
+from output_parsers import person_intel_parser, PersonIntel
 from third_parties.linkedin import scrape_linkedin_profile
 from third_parties.twitter import scrape_user_tweets
 
 
-def ice_break(name: str) -> str:
+def ice_break(name: str) -> PersonIntel:
     linkedin_profile_url = linkedin_lookup_agent(name="Eden Marco Udemy")
     linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url)
 
@@ -29,18 +30,24 @@ def ice_break(name: str) -> str:
          2. two interesting facts about them
          3. A topic that may interest them
          4. 2 creative ice breakers to pen a conversation with them
+         \n{format_instructions}
      """
 
     summary_prompt_template = PromptTemplate(
         input_variables=["linkedin_information"],
         template=summary_template,
+        partial_variables={
+            "format_instructions": person_intel_parser.get_format_instructions()
+        },
     )
 
     llm = ChatOpenAI(temperature=1, model_name="gpt-3.5-turbo")
 
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
 
-    print(chain.run(linkedin_information=linkedin_data))
+    result = chain.run(linkedin_information=linkedin_data)
+    print(result)
+    return result
 
 
 if __name__ == "__main__":
